65ee5fe613e0d5e2f082108ab352a2d4034e3486
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-21 14:13:13 -0400

    Ejemplo

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,29 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
+          
+            abrirconexion();
+            Console.ReadKey();
+        }
+        private static void abrirconexion()
+        {
+            string connectionString = GetConnectionString();
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+              //  conexion.Open();
+
+                Console.WriteLine("Estado de conexion :"+ conexion.State);
+                Console.WriteLine("Conexion: " + conexion.ConnectionString);
+            }
+        }
+
+        static private string GetConnectionString()
+        {
+          
+            return "Data Source=(local);Initial Catalog=AdventureWorks;"
+                + "Integrated Security=SSPI;";
         }
     }
+
 }
