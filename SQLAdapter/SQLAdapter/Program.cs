7abfb673f37bf533b87f3be70899dcbebd0ca1c4
Anthony  Santana <anthony26uu@gmail.com>
2017-01-21 15:38:44 -0400

    Modificacion

---
+++ SQLAdapter/SQLAdapter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace SQLAdapter
+{
+
+
+
+    class Program
+    {
+        static void Main()
+        
+              
+        {
+            SqlConnection con = new SqlConnection(cadenaConexion);
+            SqlCommand com = new SqlCommand("LISTAR_CAMPANAS", con);
+            com.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            con.Open();
+
+            SqlDataReader drCampanas = da.SelectCommand.ExecuteReader();
+            List listaCampanas = new List();
+
+            while (drCampanas.Read())
+            {
+                listaCampanas.Add(Cargar(drCampanas));
+            }
+
+            drCampanas.Close();
+            con.Close();
+            return listaCampanas;
+        }
+
+    }
+      
+    }
+
+
+
+
